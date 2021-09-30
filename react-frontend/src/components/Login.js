b59a95c5ef934875f951571ed8dd7b605ceeea06
Petkov,Stanislav S.G <s.petkov@student.fontys.nl>
2021-10-01 01:03:55 +0200

    initial navbar/basic page components

---
+++ react-frontend/src/components/Login.js
@@ -0,0 +1,11 @@
+//import {Form, Button} from 'react-bootstrap'
+
+const Login = () => {
+    return (
+        <div>
+            Login Page
+        </div>
+    )
+}
+
+export  default Login;
---
+++ react-frontend/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import '../styles/Navbar.css'
+import logo from "../images/companyLogo.png";
+
+import {Navbar, Nav} from 'react-bootstrap'
+
+const BootstrapNavbar = () => {
+    return (
+        <div className="Navbar">
+            <Navbar bg="dark" expand="lg" sticky="top">
+                <Navbar.Brand href="/"> <img src={logo} alt="Logo" style={{width: 75, marginTop: -7}}/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto" >
+                        <Nav.Link href="/" >Home</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+}
+export default BootstrapNavbar;
