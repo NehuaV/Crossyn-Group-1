ff652e4631f3c8cb5490be8f5c9c15a05e997364
NehuaV <79987000+NehuaV@users.noreply.github.com>
2021-10-04 00:32:01 +0200

    Login Functionality

--- react-frontend/src/components/Login.js
+++ react-frontend/src/components/Login.js
@@ -1,24 +1,75 @@
-
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import axios from "axios";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import '../styles/Login.css';
 
 
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginErrorMessage, setLoginErrorMessage] = useState('');
+
+    const history = useHistory();
+
+    const login = (username, password) => {
+        axios.post('http://localhost:8080/users',
+            {username, password}).then(res => {
+            if (res.data.error) {
+                console.log(res.data);
+                alert('Invalid credentials');
+            } else {
+                console.log(res.data)
+                history.push("/");
+
+                // TO DO:
+                // implement authentication -- loggedIn state/accessToken
+                // auto redirect to home page via authentication setup
+            }
+        })
+    }
+
+
+    const handleUsernameChange = (e) => {
+        e.preventDefault();
+
+        setUsername(e.target.value);
+    }
+    const handlePasswordChange = (e) => {
+        e.preventDefault();
+
+        setPassword(e.target.value);
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!username || !password) {
+            setLoginErrorMessage('Please fill in all the required fields.')
+            return;
+        } else {
+            setLoginErrorMessage('')
+        }
+        login(username, password)
+    }
+
     return (
         <div>
             <h1 className="text-center">
                 Login Page
             </h1>
+            <p className='error-msg'>{loginErrorMessage}</p>
             <div className="spacer">
                 <div className="login-container">
-                    <Form className='login-form'>
+                    <Form onSubmit={handleFormSubmit} className='login-form'>
                         <FormGroup>
                             <Label>Username</Label>
-                            <Input type='username' placeholder='Username' />
+                            <Input className="login-input" onChange={handleUsernameChange} placeholder='Username' value={username} type='text' />
                             <Label>Password</Label>
-                            <Input type='password' placeholder='Password' />
+                            <Input className="login-input" onChange={handlePasswordChange} placeholder='Password' value={password} type='password' />
                         </FormGroup>
-                        <Button>Login</Button>
+                        <Button type='submit' value='Sign In'>Login</Button>
                         <div className="extra-links">
                             <a href="/register">Signup Now!</a>
                             <a> | </a>
