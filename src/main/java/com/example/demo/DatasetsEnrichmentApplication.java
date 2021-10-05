91378166378303a9230546fa0b77c3a036d7ce3b
Roblord16 <enuta_r@yahoo.com>
2021-10-06 01:34:02 +0200

    TripCollector trip list - adds same trip to the list everytime

--- src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
+++ src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.example.demo.Main;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DatasetsEnrichmentApplication {
 
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(DatasetsEnrichmentApplication.class, args);
 
+    }
 
 }
