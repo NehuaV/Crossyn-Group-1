fee7cc06b4cf7d1e9e3e3a5168992747e76cb104
NehuaV <79987000+NehuaV@users.noreply.github.com>
2021-11-02 17:36:20 +0100

    Added Average Speed and Trip Duration

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -1,15 +1,25 @@
 package com.example.demo;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.example.demo.models.TripObject;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.example.demo.DataLayer.DataManager;
+
 public class Test {
-    public static void main(String[] args) throws Exception {
+    // Geocoding Code
+  /*  public static void main(String[] args) throws Exception {
 
         // HTTP Get request - Uses Stringify method to make custom URL with provided coordinates
         URL request = new URL(Stringify("51.59143","4.77158"));
@@ -46,6 +56,52 @@ public class Test {
     public static String Stringify(String lat,String lon){
         String key= "AIzaSyAj3JM4M64fttwnz7rnyi7SgWKzZUpgcGU";
         return "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&key="+key;
-    }
+    } */
 
+    // Code for calculating Duration
+  /*
+    public static void main(String[] args) {
+
+        String dateStart = "2021-08-30T13:43:54+02:00".replaceAll("\\s","");
+        String dateEnd = "2021-08-30T14:04:06+02:00".replaceAll("\\s","");
+
+        ZonedDateTime timeStart = ZonedDateTime.parse(dateStart);
+        ZonedDateTime timeEnd = ZonedDateTime.parse(dateEnd);
+
+        Duration duration = Duration.between(timeStart,timeEnd);
+        long s = duration.getSeconds();
+        String formattedTime = String.format("%02d:%02d:%02d", s / 3600, (s % 3600) / 60, (s % 60));
+        System.out.println(formattedTime);
+    } */
+
+    // Calculating Distance
+    public static void main(String[] args) throws IOException {
+        List<TripObject> t = new ArrayList<>();
+
+        String vehicleId = "";
+        Double lat = (double)1;
+        Double lon = (double)1;
+        int alt = 1;
+        String dateTime = ZonedDateTime.now().toString();
+        int speed = 123;
+        int speedLimit = 123;
+        Byte roadType = 1; /* there are only 6 types of roads */
+        Boolean ignition = false;
+
+        t.add(new TripObject(vehicleId,lat,lon,alt,dateTime,1,speedLimit,roadType,ignition));
+        t.add(new TripObject(vehicleId,lat,lon,alt,dateTime,2,speedLimit,roadType,ignition));
+        t.add(new TripObject(vehicleId,lat,lon,alt,dateTime,3,speedLimit,roadType,ignition));
+        t.add(new TripObject(vehicleId,lat,lon,alt,dateTime,6,speedLimit,roadType,ignition));
+        t.add(new TripObject(vehicleId,lat,lon,alt,dateTime,8,speedLimit,roadType,ignition));
+        t.add(new TripObject(vehicleId,lat,lon,alt,dateTime,2,speedLimit,roadType,ignition));
+        t.add(new TripObject(vehicleId,lat,lon,alt,dateTime,12,speedLimit,roadType,ignition));
+
+        double avgSpeed = 0;
+        for(TripObject trip : t){
+            avgSpeed += trip.getSpeed();
+        }
+        avgSpeed = avgSpeed/t.size();
+        System.out.println(avgSpeed);
+
+    }
 }
--- src/main/java/com/example/demo/models/Trip.java
+++ src/main/java/com/example/demo/models/Trip.java
@@ -3,6 +3,8 @@ package com.example.demo.models;
 import com.example.demo.LogicLayer.AddressFinder;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public class Trip {
@@ -23,6 +25,8 @@ public class Trip {
         this.tripdata = tripdata;
         this.addressFinder = new AddressFinder();
         calculateDistance();
+        calculateDuration();
+        calculateAverageSpeed();
         /*
            Enable for Testing when we have a list of [TripObject]'s
            Keep in mind we do not have unlimited uses for the Google API
@@ -34,11 +38,56 @@ public class Trip {
 
     public void StartEndAddress() throws IOException {
         if (tripdata != null && !tripdata.isEmpty()) {
-            TripObject last = tripdata.get(tripdata.size()-1);
+
+            // Get First and last item from List
             TripObject first = tripdata.get(0);
+            TripObject last = tripdata.get(tripdata.size()-1);
+
+            // Call API class AddressFinder and use the objects' coordinates to find the address
             this.startpoint = addressFinder.FindAddress(last.getLat().toString(),last.getLon().toString());
             this.endpoint = addressFinder.FindAddress(first.getLat().toString(),first.getLon().toString());
         }
+    }
+
+    public void calculateDuration(){
+        if(tripdata != null && !tripdata.isEmpty()){
+
+            // Get First and last item from List
+            TripObject first = tripdata.get(0);
+            TripObject last = tripdata.get(tripdata.size()-1);
+
+            // Get Datetime from Both Objects
+            String dateStart = first.getDateTime().replaceAll("\\s","");
+            String dateEnd = last.getDateTime().replaceAll("\\s","");
+
+            // Format them into ZonedDateTime Format
+            ZonedDateTime timeStart = ZonedDateTime.parse(dateStart);
+            ZonedDateTime timeEnd = ZonedDateTime.parse(dateEnd);
+
+            // Find the Duration between the two Times
+            Duration duration = Duration.between(timeStart,timeEnd);
+            long s = duration.getSeconds();
+            double d = (double)s;
+            /*
+               If we want we can format the time into a string with (Hour:Minute:Second) format,
+               but that would require changing the duration variable type.
+            */
+            //String formattedTime = String.format("%02d:%02d:%02d", s / 3600, (s % 3600) / 60, (s % 60));
+            this.duration = d;
+        }
+    }
+
+    public void calculateAverageSpeed(){
+
+        // Go through trip data and add all Speeds into avgSpeed
+        double avgSpeed = 0;
+        for(TripObject trip : tripdata){
+            avgSpeed += trip.getSpeed();
+        }
+
+        // Average the sum of the speeds by the size of the array
+        avgSpeed = avgSpeed/tripdata.size();
+        this.average_speed = avgSpeed;
     }
 
     public void calculateDistance()
