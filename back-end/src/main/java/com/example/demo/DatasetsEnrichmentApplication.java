a411cf97b7a277ac649e2eca6d988adae2fc282f
Roblord16 <enuta_r@yahoo.com>
2021-11-16 23:34:20 +0100

    trip splitter merge fix

--- back-end/src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
+++ back-end/src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.LogicLayer.TripManager;
-<<<<<<< HEAD
-=======
 import com.example.demo.models.DataLine;
->>>>>>> 9a1b8a17c7ee59960b41bd4151ae47eefbb119bd
 import com.example.demo.models.Trip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,14 +15,6 @@ public class DatasetsEnrichmentApplication {
     public static void main(String[] args) throws IOException {
         SpringApplication.run(DatasetsEnrichmentApplication.class, args);
 
-<<<<<<< HEAD
-        TripManager tripManager = new TripManager("dataset3.txt");
-        List<Trip> trips = tripManager.getTrips();
-        System.out.println(trips.size());
-        for (int i = 0; i < trips.size(); i++) {
-            System.out.println(trips.get(i));
-        }
-=======
 //        // Following code is only used for testing purposes
 //
 //        // Input the name of the dataset you want to split
@@ -38,14 +27,13 @@ public class DatasetsEnrichmentApplication {
 //        }
 //
 //        // Get the data lines of the trip you want to analyze via his index
-//        List<DataLine> dataLines = tripManager.getTripObjects().get(0).getTripLines();
+//        List<DataLine> dataLines = tripManager.getTripObjects().get(1).getTripLines();
 //        for (DataLine dataLine : dataLines){
 //            System.out.println(dataLine.toString());
 //        }
 //        // Get the last data line of the trip
 //        DataLine lastLine = dataLines.get(dataLines.size() - 1);
 //        System.out.println(lastLine);
->>>>>>> 9a1b8a17c7ee59960b41bd4151ae47eefbb119bd
 
     }
 
--- back-end/src/main/java/com/example/demo/LogicLayer/TripCreator.java
+++ back-end/src/main/java/com/example/demo/LogicLayer/TripCreator.java
@@ -26,64 +26,21 @@ public class TripCreator {
         this.trips = new ArrayList<>();
     }
 
-    public void Splitter(List<DataLine> data1) throws IOException {
+    public void Splitter(List<DataLine> dataLines) throws IOException {
 
         int last_index;
         int first_index = 0;
-        int index = 1;
-        int counter = 1;
 
-        //list of road types that could have safe stops
-        ArrayList<Integer> roadTypes = new ArrayList<Integer>();
-        roadTypes.add(2);
-        roadTypes.add(3);
-        roadTypes.add(4);
-        roadTypes.add(5);
-
-        //speed safety limit
-        int speedLimit = 51;
-
-//        for (DataLine dataLine : data) {
-//            //check for safe stopping spots
-////            if (roadTypes.contains(dataLine.getRoadType()) && dataLine.getSpeedLimit() <= speedLimit) {
-////            }
-//            //difference between this entry and the next time in minutes
-//            long minutes = ChronoUnit.MINUTES.between(this.getLocalDateTime(dataLine.getDateTime()), this.getLocalDateTime(data.get(index).getDateTime()));
-//            // if time difference between two data lines is bigger than 2 minutes => make a new trip
-//            Integer temp = dataLine.getSpeedLimit();
-//                if ((minutes > 2 || counter == data.size()) || temp.compareTo(speedLimit) < 0) {
-//
-//                    // Last index of the trip is the current dataLine
-//                    last_index = data.indexOf(dataLine);
-//
-//                    // List of data for one trip
-//                    List<DataLine> tripLines = data.subList(first_index, last_index + 1);
-//
-//                    // Creates a new trip and adds it to the collection of trips
-//                    this.CreateTrip(tripLines);
-//
-//                    // Sets the first index for the next trip
-//                    first_index = last_index + 1;
-//                }
-//            if (index < data.size() - 1) {
-//                index++;
-//            }
-//            counter++;
-//        }
-
-        List<DataLine> dataLines = data1;
         for (int i = 1; i < dataLines.size(); i++){
             //check for safe stopping spots
-            if (roadTypes.contains(dataLines.get(i).getRoadType().intValue()) && dataLines.get(i).getSpeedLimit() <= speedLimit) {
-                //difference between this entry and the next time in minutes
-                long minutes = ChronoUnit.MINUTES.between(this.getLocalDateTime(dataLines.get(i-1).getDateTime()), this.getLocalDateTime(dataLines.get(i).getDateTime()));
+            if (CheckSafeSpot(dataLines.get(i))) {
                 // if time difference between two data lines is bigger than 2 minutes => make a new trip
-                if (minutes > 2 || i+1 == dataLines.size()) {
+                if (CheckTimeDifference(dataLines.get(i-1), dataLines.get(i)) || i+1 == dataLines.size()) {
                     // Last index of the trip is the current dataLine
-                    last_index = data1.indexOf(dataLines.get(i));
+                    last_index = i;  //dataLines.indexOf(dataLines.get(i))
 
                     // List of data for one trip
-                    List<DataLine> tripLines = data1.subList(first_index, last_index + 1);
+                    List<DataLine> tripLines = dataLines.subList(first_index, last_index);
 
                     // Creates a new trip and adds it to the collection of trips
                     this.CreateTrip(tripLines);
@@ -94,6 +51,31 @@ public class TripCreator {
             }
         }
     }
+    //check if the location is a safe parking spot
+    private boolean CheckSafeSpot(DataLine dataLine){
+        //list of road types that could have safe stops
+        ArrayList<Integer> roadTypes = new ArrayList<Integer>();
+        roadTypes.add(2);
+        roadTypes.add(3);
+        roadTypes.add(4);
+        roadTypes.add(5);
+
+        //speed safety limit
+        int speedLimit = 51;
+
+        if (roadTypes.contains(dataLine.getRoadType().intValue()) && dataLine.getSpeedLimit() <= speedLimit) { return true; }
+
+        return false;
+    }
+    //check if the time difference is bigger than 2 minutes
+    private boolean CheckTimeDifference(DataLine previousLine, DataLine currentLine){
+
+        //difference between this entry and the next time in minutes
+        long minutes = ChronoUnit.MINUTES.between(this.getLocalDateTime(previousLine.getDateTime()), this.getLocalDateTime(currentLine.getDateTime()));
+        return minutes>2;
+    }
+
+
 
     @SneakyThrows
     public void CreateTrip(List<DataLine> tripLines) throws IOException {
@@ -110,5 +92,7 @@ public class TripCreator {
     public LocalDateTime getLocalDateTime(String dateTime) {
         return LocalDateTime.parse(dateTime.substring(0, dateTime.lastIndexOf("+02:00")));
     }
+
+
 
 }
