7f67b103daffde37f3bfa484784e23d7a371a3a2
Roblord16 <enuta_r@yahoo.com>
2021-11-16 22:44:38 +0100

    updated trip splitting to account to speed limit and road type

--- back-end/src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
+++ back-end/src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
@@ -1,15 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.LogicLayer.TripManager;
+import com.example.demo.models.Trip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class DatasetsEnrichmentApplication {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(DatasetsEnrichmentApplication.class, args);
+
+        TripManager tripManager = new TripManager("dataset3.txt");
+        List<Trip> trips = tripManager.getTrips();
+        System.out.println(trips.size());
+        for (int i = 0; i < trips.size(); i++) {
+            System.out.println(trips.get(i));
+        }
 
     }
 
--- back-end/src/main/java/com/example/demo/models/DataLine.java
+++ back-end/src/main/java/com/example/demo/models/DataLine.java
@@ -62,4 +62,5 @@ public class DataLine {
         this.ignition = ignition;
     }
 
+
 }
