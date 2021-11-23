b3a1ccbc87aa2e54cf38ad099abd3255958df95c
Roblord16 <enuta_r@yahoo.com>
2021-11-23 14:32:01 +0100

    trip splitting ajustment

--- back-end/src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
+++ back-end/src/main/java/com/example/demo/DatasetsEnrichmentApplication.java
@@ -18,7 +18,7 @@ public class DatasetsEnrichmentApplication {
         // // Following code is only used for testing purposes
 
         // // Input the name of the dataset you want to split
-        // TripManager tripManager = new TripManager("dataset1.txt");
+//         TripManager tripManager = new TripManager("dataset7.txt");
 
         // // Get the all the split trips and display them in the console
         // List<Trip> trips = tripManager.getTrips();
@@ -26,15 +26,24 @@ public class DatasetsEnrichmentApplication {
         //     System.out.println(trip.toString());
         // }
 
-        // // Get the data lines of the trip you want to analyze via his index
-        // List<DataLine> dataLines = tripManager.getTripObjects().get(1).getTripLines();
-        // for (DataLine dataLine : dataLines){
-        //     System.out.println(dataLine.toString());
-        // }
+//         // Get the data lines of the trip you want to analyze via his index
+//         List<DataLine> dataLines = tripManager.getTripObjects().get(1).getTripLines();
+//         for (DataLine dataLine : dataLines){
+//             System.out.println(dataLine.toString());
+//         }
 //        // Get the last data line of the trip
 //        DataLine lastLine = dataLines.get(dataLines.size() - 1);
 //        System.out.println(lastLine);
 
+//        //Show number of total trips made from the set
+//        System.out.println(tripManager.getTrips().size());
+//
+//        //Get the first and last line of each trip
+//        for (int i = 0; i<tripManager.getTrips().size(); i++) {
+//            System.out.println("trip nr" + i + ":");
+//            System.out.println(tripManager.getTripObjects().get(i).getTripLines().get(0));
+//            System.out.println(tripManager.getTripObjects().get(i).getTripLines().get(tripManager.getTripObjects().get(i).getTripLines().size()-1));
+//        }
     }
 
 }
