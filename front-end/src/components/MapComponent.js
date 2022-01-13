045592ad861314c5c561bde5e377bbef1a5790d3
stassig <stassig164@gmail.com>
2022-01-13 18:13:52 +0200

    Fixing MapComponent Rendering

--- front-end/src/components/MapComponent.js
+++ front-end/src/components/MapComponent.js
@@ -1,64 +1,66 @@
 import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  Polyline,
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+    Polyline,
 } from "react-leaflet";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../styles/MapComponent.css";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import {useParams} from "react-router-dom/cjs/react-router-dom.min";
 // Hardcoded dataset
 import Trip1 from "../testing set/trip1.json";
 import Trip2 from "../testing set/trip2.json";
 import Trip3 from "../testing set/trip3.json";
+import axios from "axios";
 
 function MapComponent() {
 
-  const { tripId } = useParams();
+    const {tripId} = useParams();
+    const [result, setResult] = useState(null);
 
-  var result = null;
-
-  if (tripId == 1 || tripId == 4) {
-    result = Trip1.map((a) => [a.lat, a.lon]);
-  } else if (tripId == 2 || tripId == 5) {
-    result = Trip2.map((a) => [a.lat, a.lon]);
-  } else {
-    result = Trip3.map((a) => [a.lat, a.lon]);
-  }
+    useEffect(() => {
+        const loadTrip = async () => {
+            const response = await axios.get(`trips/${tripId}`);
+            setResult(response.data.datalines.map((a) => [a.lat, a.lon]));
+        }
+        loadTrip();
+    }, []);
 
 
-  // // Converts JSON Object Array To Nested Coordinate Array
-  // var result = DataSet.map((a) => [a.lat, a.lon]);
+    // // Converts JSON Object Array To Nested Coordinate Array
+    // var result = DataSet.map((a) => [a.lat, a.lon]);
 
-  // Color of the path
-  const colorOptions = { color: "red", width: "3" };
-  // Use Math to decide  the middle point of a trip
-  var middle = result[Math.floor(result.length / 2)];
+    // Color of the path
+    const colorOptions = {color: "red", width: "3"};
 
-  return (
-    <>
-      <h1>Trip #{tripId}</h1>
-      <MapContainer
-        center={result[0]}
-        zoom={13}
-        scrollWheelZoom={true}
-        animate={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={result[0]}>
-          <Popup>START OF THE TRIP</Popup>
-        </Marker>
-        <Marker position={result[result.length - 1]}>
-          <Popup>END OF THE TRIP</Popup>
-        </Marker>
-        <Polyline pathOptions={colorOptions} positions={result} />
-      </MapContainer>
-    </>
-  );
+
+    return (!result ? null :
+            <>
+                <h1>Trip #{tripId}</h1>
+                <MapContainer
+                    center={result[Math.floor(result.length / 2)]}
+                    zoom={9}
+                    scrollWheelZoom={true}
+                    animate={true}
+                >
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={result[0]}>
+                        <Popup>START OF THE TRIP</Popup>
+                    </Marker>
+                    <Marker position={result[result.length - 1]}>
+                        <Popup>END OF THE TRIP</Popup>
+                    </Marker>
+                    <Polyline pathOptions={colorOptions} positions={result}/>
+                </MapContainer>
+            </>
+
+    );
+
+
 }
 
 export default MapComponent;
